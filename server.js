cbb8e84146eb2e153469b5e1ad4f624fa59b8a12
OliverYgo <oliverzygo@duck.com>
2022-10-09 00:29:42 +0100

    Testing if the static files worked on dynamic ejs files. They do.

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 
+app.get('/test', (req, res) => {
+    res.render('test', {text: 'Hello there, this is the test page.'})
+})
+
 app.listen(port, () => {
     console.log('Server started on port', port)
 })
