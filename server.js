c430ad294610e23f900ae40de6b72e0e6485be5b
OliverYgo <oliverzygo@duck.com>
2022-10-08 23:25:47 +0100

    index rendered

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
+const port = 5000
 
+app.set('view engine', 'ejs')
 
+app.get('/', (req, res) => {
+    res.render('index')
+})
 
-app.listen(5000)
+app.listen(port, () => {
+    console.log('Server started on port', port)
+})
