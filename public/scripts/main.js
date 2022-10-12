75c6e06bebffd749bd03fc7cd188441fd4dbe48c
OliverYgo <oliverzygo@duck.com>
2022-10-12 14:33:55 +0100

    The current page link highlighted in  the nav now changes adaptively.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,19 +3,57 @@ let navActive = false;
 
 pauseScrolling = () => {
     document.body.classList.toggle('paused-scrolling')
+    console.log('toggled')
 }
 
 const header = document.querySelector('header')
 const navToggle = document.querySelector('.nav-toggle')
 const navItems = document.querySelector('.nav-items')
 const navItem = document.querySelectorAll('.nav-item')
-const heroSec = document.querySelector('#hero')
+const navLinks = document.querySelectorAll('.nav-link')
+const sections = document.querySelectorAll('section')
+let currentLink = 0
+
+// Change the highlighted nav link
+section_count = 0
+sections.forEach(section => {
+  section.dataset.index = section_count
+  ++section_count
+})
+
+function changeCurrentLink() {
+  sections.forEach(section => {
+    if (window.pageYOffset < section.offsetTop) return
+    currentLink = section.dataset.index
+  })
+
+  navLinks.forEach(navLink => {
+    navLink.classList.remove('current-link')
+  })
+
+  navLinks[currentLink].classList.add('current-link')
+}
+
+// show/hide header on scroll
+
+let prevScrollpos = window.pageYOffset;
+
+function showHideHeader() {
+  let currentScrollPos = window.pageYOffset;
+
+  if (prevScrollpos > currentScrollPos) {
+    header.style.top = "0";
+  } else {
+    header.style.top = "-60px";
+  }
+  
+  prevScrollpos = currentScrollPos;
+}
 
 toggleNav = () => {
     header.classList.toggle('nav-active')
     navToggle.classList.toggle('nav-active')
     navItems.classList.toggle('nav-active')
-    heroSec.classList.toggle('nav-active')
     navActive = !navActive
     pauseScrolling()
 }
@@ -26,22 +64,14 @@ navToggle.addEventListener('click', () => {
 
 navItem.forEach(element => {
     element.addEventListener('click', () => {
+        changeCurrentLink()
         toggleNav()
     })
 });
 
-// show/hide header on scroll
-
-let prevScrollpos = window.pageYOffset;
-
+// Handles page scroll events
 window.onscroll = function() {
-  let currentScrollPos = window.pageYOffset;
-
-  if (prevScrollpos > currentScrollPos) {
-    header.style.top = "0";
-  } else {
-    header.style.top = "-60px";
-  }
-  
-  prevScrollpos = currentScrollPos;
+  showHideHeader()
+  // Changes the current nav link highlight
+  changeCurrentLink()
 }
